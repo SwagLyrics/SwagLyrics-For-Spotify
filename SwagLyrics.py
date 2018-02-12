9ff36d67e6b6cd2f2047ab2a9dde6cee611f3796
Aadi Bajpai <aadibajpai@gmail.com>
2018-02-12 21:53:32 +0530

    Support more songs + loading spinner + more tests

--- SwagLyrics.py
+++ SwagLyrics.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import spotify
 import re
+import sys
+import time
 
 song = spotify.song()
 artist = spotify.artist()
@@ -9,6 +11,7 @@ artist = spotify.artist()
 
 def stripper(song, artist):
 	song = re.sub(r'\([^)]*\)', '', song).strip()
+	song = re.sub('\- .*', '', song).strip()
 	song_data = artist + '-' + song
 	url_data = song_data.replace(' ', '-')
 	for ch in [',', "'", '!']:
@@ -24,7 +27,7 @@ def get_lyrics(song, artist):
 	html = BeautifulSoup(page.text, "html.parser")
 	lyrics_path = html.find("div", class_="lyrics")
 	if lyrics_path is None:
-		lyrics = 'Could not get lyrics for {song} by {artist}.'.format(song=song, artist=artist)
+		lyrics = 'Couldn\'t get lyrics for {song} by {artist}.'.format(song=song, artist=artist)
 	else:
 		lyrics = lyrics_path.get_text().encode('ascii', 'ignore').decode('utf-8')
 	return lyrics
@@ -37,5 +40,21 @@ def lyrics(song, artist):
 		return 'Nothing playing at the moment.'
 
 
+def spinning_cursor():
+	while True:
+		for cursor in '|/-\\':
+			yield cursor
+
+
+spinner = spinning_cursor()
+
 if __name__ == "__main__":
+	print('Getting lyrics for {song} by {artist} '.format(song=song, artist=artist), end='')
+	for _ in range(30):
+		sys.stdout.write(next(spinner))
+		sys.stdout.flush()
+		time.sleep(0.1)
+		sys.stdout.write('\b')
+	sys.stdout.write('\b   \n')
+	sys.stdout.flush()
 	print(lyrics(song, artist))
