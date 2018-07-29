41c162fa2eb4162a9c0fd2bd8d87ab3760c50871
Aadi Bajpai <aadibajpai@gmail.com>
2018-07-30 01:21:48 +0530

    Make lyrics autoupdate

--- browser.py
+++ browser.py
@@ -5,14 +5,26 @@ import spotify
 
 app = Flask(__name__)
 
+song = None
+artist = None
+
 
 @app.route('/')
 def tab():
+	global song, artist
 	song = spotify.song()
 	artist = spotify.artist()
 	current_lyrics = lyrics(song, artist)
 	current_lyrics = current_lyrics.split('\n')
 	return render_template('lyrics.html', lyrics=current_lyrics, song=song)
+
+
+@app.route('/songChanged', methods=['GET'])
+def song_changed():
+	global song
+	if song == spotify.song():
+		return 'no'
+	return 'yes'
 
 
 if __name__ == "__main__":
