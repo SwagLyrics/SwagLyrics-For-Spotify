7cb13419ca36d72dcc235fec3f7605a6c6ffbef0
Aadi Bajpai <aadibajpai@gmail.com>
2018-08-02 04:14:35 +0530

    update binaries

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Aadi Bajpai",
     author_email="aadibajpai@gmail.com",
-    description="Display lyrics for currently playing song from Spotify as cli or a browser tab.",
+    description="Display lyrics for currently playing song from Spotify in a CLI or in browser tab.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aadibajpai/SwagLyrics-For-Spotify",
