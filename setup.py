4294aebd8120bfe4dc3cdee5f8c23faa23e1dd13
Aadi Bajpai <aadibajpai@gmail.com>
2018-08-02 18:27:42 +0530

    fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aadibajpai/SwagLyrics-For-Spotify",
-    scripts=['swaglyrics/run.py'],
+    scripts=['swaglyrics/swaglyrics.py'],
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
