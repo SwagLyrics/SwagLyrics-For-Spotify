7a7959b737f9e687ef183dce3306143cf3cbb767
Aadi Bajpai <aadibajpai@gmail.com>
2018-10-17 15:19:08 +0530

    add documentation

--- swaglyrics/tab.py
+++ swaglyrics/tab.py
@@ -4,6 +4,7 @@ import os
 from swaglyrics import spotify
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath('tab.py')), 'templates'))
+# use relative path of the template folder
 
 song = None
 artist = None
@@ -11,21 +12,23 @@ artist = None
 
 @app.route('/')
 def tab():
-	global song, artist
-	song = spotify.song()
-	artist = spotify.artist()
-	current_lyrics = lyrics(song, artist)
-	current_lyrics = current_lyrics.split('\n')
-	return render_template('lyrics.html', lyrics=current_lyrics, song=song, artist=artist)
+    # format lyrics for the browser tab template
+    global song, artist
+    song = spotify.song()
+    artist = spotify.artist()
+    current_lyrics = lyrics(song, artist)
+    current_lyrics = current_lyrics.split('\n')  # break lyrics line by line
+    return render_template('lyrics.html', lyrics=current_lyrics, song=song, artist=artist)
 
 
 @app.route('/songChanged', methods=['GET'])
 def song_changed():
-	global song
-	if song == spotify.song() or spotify.song() is None:
-		return 'no'
-	return 'yes'
+    # to refresh lyrics when song changed
+    global song
+    if song == spotify.song() or spotify.song() is None:
+        return 'no'
+    return 'yes'
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
