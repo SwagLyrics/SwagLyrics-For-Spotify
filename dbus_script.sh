ec262dd11c8937f08776ae6a9a54a5a53d05c88a
T1duS <udit121sanghi@gmail.com>
2018-11-19 16:02:04 +0530

    Trying a script for dbus

--- dbus_script.sh
+++ dbus_script.sh
@@ -1,26 +1,3 @@
-echo Trying to find python version...
-
-python_version=`python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'`
-
-case "$python_version" in
-  "3.4"*)
-    system_python=python3.4
-    ;;
-  "3.3"*)
-    system_python=python3.3
-    ;;
-  "3.2"*)
-    system_python=python3.2
-    ;;
-  *)
-    echo Python version was not understood. It was detected as - $python_version
-    ;;
-esac
-
-sudo apt-get install ${system_python}-dev
-
-sh .install.dbus.sh
-
 echo Downloading dbus...
 wget http://dbus.freedesktop.org/releases/dbus/dbus-1.6.30.tar.gz -O dbus.tar.gz -q
 echo Unpacking dbus...
