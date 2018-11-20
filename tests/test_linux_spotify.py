491517b7cfbf8938a87a0743366b1f84931b80b8
T1duS <udit121sanghi@gmail.com>
2018-11-21 00:59:37 +0530

    Added tests for linux support

---
+++ tests/test_linux_spotify.py
@@ -0,0 +1,41 @@
+"""
+Contains unit tests for spotify.py for linux
+"""
+import unittest
+from swaglyrics.spotify import get_info_linux
+from mock import mock,patch
+
+class Tests(unittest.TestCase):
+	"""
+	Unit tests
+	"""
+
+	def setup(self):
+		pass
+
+	@mock.patch('dbus.SessionBus')
+	def test_get_info_linux_works(self, mock_dbus):
+		"""
+		test that get_info_linux function doesn't give errors and returns a tuple
+		"""
+		self.assertEqual(type(get_info_linux()), type(()))
+
+	@patch('swaglyrics.spotify.get_info_linux')
+	def test_that_artist_function_calls_get_info(self, mock):
+		"""
+		test that test artist function calls get_info_linux function
+		"""
+		x = artist()
+		self.assertTrue(mock.called)
+
+	@patch('swaglyrics.spotify.get_info_linux')
+	def test_that_song_function_calls_get_info(self, mock):
+		"""
+		test that test song function calls get_info_linux function
+		"""
+		x = song()
+		self.assertTrue(mock.called)
+
+
+if __name__ == '__main__':
+	unittest.main()
