22d765aa1ada25cabce4d400fc1bfa1571740946
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-11-26 23:17:52 +0530

    Update spotify.py

--- swaglyrics/spotify.py
+++ swaglyrics/spotify.py
@@ -30,35 +30,41 @@ def get_info_windows():
 
 
 def get_info_linux():
-	import dbus
+	try:
+		import dbus
 
-	session_bus = dbus.SessionBus()
-	spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
-										"/org/mpris/MediaPlayer2")
-	spotify_properties = dbus.Interface(spotify_bus,
-	                                    "org.freedesktop.DBus.Properties")
-	metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-	track = str(metadata['xesam:title'])
-	artist = str(metadata['xesam:artist'][0])
-	return artist, track
+		session_bus = dbus.SessionBus()
+		spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+											"/org/mpris/MediaPlayer2")
+		spotify_properties = dbus.Interface(spotify_bus,
+						    "org.freedesktop.DBus.Properties")
+		metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+		track = str(metadata['xesam:title'])
+		artist = str(metadata['xesam:artist'][0])
+		return artist, track
+	except:
+		return None
 
 
 def get_info_mac():
-	from Foundation import NSAppleScript
-	apple_script_code = """
-	getCurrentlyPlayingTrack()
-	on getCurrentlyPlayingTrack()
-		tell application "Spotify"
-			set currentArtist to artist of current track as string
-			set currentTrack to name of current track as string
-			return {currentArtist, currentTrack}
-		end tell
-	end getCurrentlyPlayingTrack
-	"""
-	s = NSAppleScript.alloc().initWithSource_(apple_script_code)
-	x = s.executeAndReturnError_(None)
-	a = str(x[0]).split('"')
-	return a[1], a[3]
+	try:
+		from Foundation import NSAppleScript
+		apple_script_code = """
+		getCurrentlyPlayingTrack()
+		on getCurrentlyPlayingTrack()
+			tell application "Spotify"
+				set currentArtist to artist of current track as string
+				set currentTrack to name of current track as string
+				return {currentArtist, currentTrack}
+			end tell
+		end getCurrentlyPlayingTrack
+		"""
+		s = NSAppleScript.alloc().initWithSource_(apple_script_code)
+		x = s.executeAndReturnError_(None)
+		a = str(x[0]).split('"')
+		return a[1], a[3]
+	except:
+		return None
 
 
 def artist():
