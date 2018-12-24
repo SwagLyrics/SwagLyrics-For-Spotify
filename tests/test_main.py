00136fb677790f66499028ba1d9322311ef0f278
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-12-24 10:06:38 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -44,7 +44,7 @@ class Tests(unittest.TestCase):
         self.assertTrue(mock_app.called)
 
     @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=False, cli=True))
-    @mock.patch('time.sleep', side_effect=exit)
+    @mock.patch('time.sleep', side_effect=KeyboardInterrupt)
     def test_parser_runs_cli(self, mock_time, mock_argparse):
         """
         Tests whether parser runs cli
