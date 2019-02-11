b9bd3ab647bf20f78fa4eee9b0f7fb1c70877951
tushar <tushar@linux.testdomain.com>
2019-02-11 21:19:04 +0530

    Add support for Lyricsmode.com and Google

--- swaglyrics/cli.py
+++ swaglyrics/cli.py
@@ -11,7 +11,7 @@ def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')  # clear command window
 
 
-def stripper(song, artist):
+def stripper_genius(song, artist):
 	"""
 	Generate the url path given the song and artist to format the Genius URL with.
 	Strips the song and artist of special characters and unresolved text such as 'feat.' or text within braces.
@@ -37,17 +37,58 @@ def stripper(song, artist):
 	url_data = unidecode(url_data)  # remove accents and other diacritics
 	return url_data
 
+def stripper_lyricsmode(song, artist):
+	"""
+	Generate the url path given the song and artist to format the LYRICSMODE URL with.
+	Strips the song and artist of special characters and unresolved text such as 'feat.' or text within braces.
+	Then concatenates both with hyphens replacing the blank spaces.
+	Eg.
+	>>>stripper('Paradise City', 'Guns n’ Roses')
+	g/guns_n_roses/paradise_city.html
+	Which then formats the url to https://lyricsmode.com/lyrics/g/guns_n_roses/paradise_city.html
+	:param song: currently playing song
+	:param artist: song artist
+	:return: formatted url path
+	"""
+	song = re.sub(r'\([^)]*\)', '', song).strip()  # remove braces and included text
+	song = re.sub('- .*', '', song).strip()  # remove text after '- '
+	song_data = artist[0] +'/'  + artist + '/' + song
+	url_data = song_data.replace('&', 'and')
+	for ch in [',', '\'', '!', '.', '’', '"', '+', '?', 'Σ', '#']:
+		if ch in url_data:
+			url_data = url_data.replace(ch, '')
+	url_data = ' '.join(url_data.split())  # remove multiple spaces to one space
+	url_data = url_data.replace(' ', '_')  # hyphenate the words together
+	url_data = unidecode(url_data)  # remove accents and other diacritics
+	url_data = url_data + '.html'
+	return url_data.lower()
 
-def get_lyrics(song, artist, make_issue=True):
+def stripper_google(song, artist):
+	# song = re.sub(r'\([^)]*\)', '', song).strip()  # remove braces and included text
+	# song = re.sub('- .*', '', song).strip()  # remove text after '- '
+	q = song + ' ' + artist + ' lyrics'
+	q = q.replace('%', '%25')
+	dic = {',':'%2C', '\'':'%27', '+':'%2B', '?':r'%3F', '#':'%23', '&':'%26', '^':r'%5E', '$':'%24',
+		'@':'%40', '=':'%3D', '`':'%60', '{':'%7B', '}':'%7D', '[':'%5B', ']':'%5D', ':':'%3A',
+		';':'%3B', '\\':'5C', '|':'%7C', '/':r'%2F'
+	}
+	for ch, r_ch in dic.items():
+		# print(ch,)
+		q = q.replace(ch, r_ch)
+	# q = unidecode(q)
+	q = q.replace(' ', '+')
+	return q
+
+
+def get_lyrics_genius(song, artist):
 	"""
 	Get lyrics from Genius given the song and artist.
 	Formats the URL with the stripped url path to fetch the lyrics.
 	:param song: currently playing song
 	:param artist: song artist
-	:param make_issue: whether to make an issue on GitHub if song unsupported
 	:return: song lyrics
 	"""
-	url_data = stripper(song, artist)  # generate url path using stripper()
+	url_data = stripper_genius(song, artist)  # generate url path using stripper()
 	url = 'https://genius.com/{}-lyrics'.format(url_data)  # format the url with the url path
 	page = requests.get(url)
 	if page.status_code != 200:
@@ -58,21 +99,86 @@ def get_lyrics(song, artist, make_issue=True):
 	# TODO: Add error handling
 	lyrics_path = html.find("div", class_="lyrics")  # finding div on Genius containing the lyrics
 	if lyrics_path is None:
-		with open('unsupported.txt', 'a') as f:
-			f.write('{song} by {artist} \n'.format(song=song, artist=artist))
-			f.close()
-		lyrics = 'Couldn\'t get lyrics for {song} by {artist}.\n'.format(song=song, artist=artist)
-		try:
-			# Log song and artist for which lyrics couldn't be obtained
-			if make_issue:
-				r = requests.post('http://aadibajpai.pythonanywhere.com/unsupported', data={'song': song, 'artist': artist})
-				if r.status_code == 200:
-					lyrics += r.text
-		except requests.exceptions.RequestException:
-			pass
+		return None
 	else:
 		lyrics = UnicodeDammit(lyrics_path.get_text().strip()).unicode_markup
 	return lyrics
+
+
+
+def get_lyrics_lyricsmode(song, artist):
+	"""
+	Get lyrics from LYRICSMODE.COM given the song and artist.
+	Formats the URL with the stripped url path to fetch the lyrics.
+	:param song: currently playing song
+	:param artist: song artist
+	:return: song lyrics
+	"""
+	url_data = stripper_lyricsmode(song, artist)  # generate url path using stripper()
+	url = 'https://lyricsmode.com/lyrics/{}'.format(url_data)  # format the url with the url path
+	page = requests.get(url)
+	if page.status_code != 200:
+		return None
+	html = BeautifulSoup(page.text, "html.parser")
+	lyrics_path = html.find("div", class_="js-new-text-select")  # finding div on LYRICSMODE containing the lyrics
+	if lyrics_path is None:
+		return None
+	lyrics = UnicodeDammit(lyrics_path.get_text().strip()).unicode_markup
+	return lyrics
+
+def get_lyrics_google(song, artist):
+	headers_Get = {
+	    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	    'Accept-Language': 'en-US,en;q=0.5',
+	    'Accept-Encoding': 'gzip, deflate',
+	    'DNT': '1',
+	    'Connection': 'keep-alive',
+	    'Upgrade-Insecure-Requests': '1'
+	}
+	q = stripper_google(song, artist)
+	song_ = re.sub(r'\([^)]*\)', '', song).strip()  # remove braces and included text
+	song_ = re.sub('- .*', '', song_).strip()  # remove text after '- '
+
+	url = 'https://www.google.com/search?q=' + q + '&ie=utf-8&oe=utf-8'
+	# url = 'https://www.google.com/search?q={}&oq={}&aqs=chrome..69i64.10943j0j7&sourceid=chrome&ie=UTF-8'.format(url_data, url_data)
+	page = requests.get(url, headers=headers_Get)
+	if page.status_code!=200:
+		return None
+	html = BeautifulSoup(page.text, 'html.parser')
+	lyrics = html.find('div', class_='Kvw2ac')
+	song_google = html.find('div', class_="gsmt")
+	artist_google = html.find('div', class_="wwUB2c")
+	if lyrics is None or artist_google is None or song_google is None:
+		return None
+	# print(song_google.text, artist_google.text)
+	for i in artist.split():
+		if not i.lower() in artist_google.text.lower():
+			return None
+	# print(lyrics.text)
+	return UnicodeDammit(lyrics.get_text('\n').strip()).unicode_markup
+
+def unsupported_song_addition(song, artist, make_issue=True):
+	"""
+	Adds the song to the list of unsupported songs
+	:param song:
+	:param artist:
+	:param make_issue: whether to make an issue on GitHub if song unsupported
+	"""
+	with open('unsupported.txt', 'a') as f:
+		f.write('{song} by {artist} \n'.format(song=song, artist=artist))
+		f.close()
+	text = 'Couldn\'t get lyrics for {song} by {artist}.\n'.format(song=song, artist=artist)
+	try:
+		# Log song and artist for which lyrics couldn't be obtained
+		if make_issue:
+			r = requests.post('http://aadibajpai.pythonanywhere.com/unsupported', data={'song': song, 'artist': artist})
+			if r.status_code == 200:
+				text += r.text
+	except requests.exceptions.RequestException:
+		pass
+	return text
+
 
 
 def lyrics(song, artist, make_issue=True):
@@ -90,8 +196,14 @@ def lyrics(song, artist, make_issue=True):
 					return 'Lyrics unavailable for {song} by {artist}.\n'.format(song=song, artist=artist)
 		except FileNotFoundError:
 			pass
-		print('\nGetting lyrics for {song} by {artist} '.format(song=song, artist=artist), end='')
-		lyrics = get_lyrics(song, artist, make_issue)
+		# print('\nGetting lyrics for {song} by {artist} '.format(song=song, artist=artist), end='')
+		lyrics = get_lyrics_genius(song, artist)
+		if lyrics is None:
+			lyrics = get_lyrics_lyricsmode(song, artist)
+		if lyrics is None:
+			lyrics = get_lyrics_google(song, artist)
+		if lyrics is None:			
+			lyrics = unsupported_song_addition(song, artist, make_issue)
 		for _ in range(30):  # loading spinner
 			sys.stdout.write(next(spinner))
 			sys.stdout.flush()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,19 @@
-"""
-Contains unit tests for cli.py
-"""
 import unittest
-from swaglyrics.cli import stripper, lyrics, get_lyrics, clear
+from swaglyrics.cli import (stripper_genius,
+	stripper_lyricsmode,
+	stripper_google,
+	lyrics,
+	get_lyrics_genius,
+	get_lyrics_lyricsmode,
+	get_lyrics_google,
+	clear,
+	unsupported_song_addition
+)
 from mock import mock, patch
 import os
 import requests
+
+
 
 class R:
 	"""
@@ -17,28 +25,50 @@ class R:
 		self.status_code = status_code
 		self.text = text
 
-class Tests(unittest.TestCase):
+class Test(unittest.TestCase):
 	"""
-	Unit tests
+	Unit tests for the new cli
 	"""
 
 	def setup(self):
 		pass
 
-	def test_that_stripping_works(self):
-		"""
-		Test that stripping works
-		"""
-		self.assertEqual(stripper('River (feat. Ed Sheeran)', 'Eminem'), 'Eminem-River')
+	def test_that_all_the_strippers_work(self):
+		## Genius
+		self.assertEqual(stripper_genius('River (feat. Ed Sheeran)', 'Eminem'), 'Eminem-River')
 		self.assertEqual(
-			stripper(
+			stripper_genius(
 				'CAN\'T STOP THE FEELING!'
 				' (Original Song from DreamWorks Animation\'s \"TROLLS\")', 'Justin Timberlake'),
 			'Justin-Timberlake-CANT-STOP-THE-FEELING')
-		self.assertEqual(stripper('Ain\'t My Fault - R3hab Remix', 'Zara Larsson'), 'Zara-Larsson-Aint-My-Fault')
-		self.assertEqual(stripper('1800-273-8255', 'Logic'), 'Logic-1800-273-8255')
-		self.assertEqual(stripper('Scream & Shout', 'will.i.am'), 'william-Scream-and-Shout')
-		self.assertEqual(stripper('Heebiejeebies - Bonus', 'Aminé'), 'Amine-Heebiejeebies')
+		self.assertEqual(stripper_genius('Ain\'t My Fault - R3hab Remix', 'Zara Larsson'), 'Zara-Larsson-Aint-My-Fault')
+		self.assertEqual(stripper_genius('1800-273-8255', 'Logic'), 'Logic-1800-273-8255')
+		self.assertEqual(stripper_genius('Scream & Shout', 'will.i.am'), 'william-Scream-and-Shout')
+		self.assertEqual(stripper_genius('Heebiejeebies - Bonus', 'Aminé'), 'Amine-Heebiejeebies')
+
+		## LyricsMode
+		self.assertEqual(stripper_lyricsmode('River (feat. Ed Sheeran)', 'Eminem'), 'e/eminem/river.html')
+		self.assertEqual(
+			stripper_lyricsmode(
+				'CAN\'T STOP THE FEELING!'
+				' (Original Song from DreamWorks Animation\'s \"TROLLS\")', 'Justin Timberlake'),
+			'j/justin_timberlake/cant_stop_the_feeling.html')
+		self.assertEqual(stripper_lyricsmode('Ain\'t My Fault - R3hab Remix', 'Zara Larsson'), 'z/zara_larsson/aint_my_fault.html')
+		self.assertEqual(stripper_lyricsmode('1800-273-8255', 'Logic'), 'l/logic/1800-273-8255.html')
+		self.assertEqual(stripper_lyricsmode('Scream & Shout', 'will.i.am'), 'w/william/scream_and_shout.html')
+		self.assertEqual(stripper_lyricsmode('Heebiejeebies - Bonus', 'Aminé'), 'a/amine/heebiejeebies.html')
+
+		##Google search
+		self.assertEqual(stripper_google('River (feat. Ed Sheeran)', 'Eminem'), 'River+(feat.+Ed+Sheeran)+Eminem+lyrics')
+		self.assertEqual(
+			stripper_google(
+				'CAN\'T STOP THE FEELING!'
+				' (Original Song from DreamWorks Animation\'s \"TROLLS\")', 'Justin Timberlake'),
+			'CAN%27T+STOP+THE+FEELING!+(Original+Song+from+DreamWorks+Animation%27s+"TROLLS")+Justin+Timberlake+lyrics')
+		self.assertEqual(stripper_google('Ain\'t My Fault - R3hab Remix', 'Zara Larsson'), 'Ain%27t+My+Fault+-+R3hab+Remix+Zara+Larsson+lyrics')
+		self.assertEqual(stripper_google('1800-273-8255', 'Logic'), '1800-273-8255+Logic+lyrics')
+		self.assertEqual(stripper_google('Scream & Shout', 'will.i.am'), 'Scream+%26+Shout+will.i.am+lyrics')
+		self.assertEqual(stripper_google('Heebiejeebies - Bonus', 'Aminé'), 'Heebiejeebies+-+Bonus+Aminé+lyrics')
 
 	def test_that_no_song_or_artist_does_not_break_stuff(self):
 		"""
@@ -50,39 +80,47 @@ class Tests(unittest.TestCase):
 
 	def test_that_get_lyrics_works(self):
 		"""
-		Test that get_lyrics function works
+		Test that get_lyrics_genius, get_lyrics_google, get_lyrics_lyricsmode functions work
 		"""
-		self.assertEqual(get_lyrics("Faded", "Alan Walker")[:9], "[Verse 1]")
-		self.assertEqual(get_lyrics("Radioactive", "Imagine Dragons")[:7], "[Intro]")
-		self.assertEqual(get_lyrics("Battle Symphony", "Linkin Park")[:9], "[Verse 1]")
+		##genius
+		self.assertEqual(get_lyrics_genius("Faded", "Alan Walker")[:9], "[Verse 1]")
+		self.assertEqual(get_lyrics_genius("Radioactive", "Imagine Dragons")[:7], "[Intro]")
+		self.assertEqual(get_lyrics_genius("Battle Symphony", "Linkin Park")[:9], "[Verse 1]")
+
+		## lyricsmode
+		self.assertEqual(get_lyrics_lyricsmode("Faded", "Alan Walker")[:9], "[Verse 1]")
+		self.assertEqual(get_lyrics_lyricsmode("Radioactive", "Imagine Dragons")[:10], "I'm waking")
+		self.assertEqual(get_lyrics_lyricsmode("Battle Symphony", "Linkin Park")[:9], "[Verse 1]")
+		##google
+		self.assertEqual(get_lyrics_google("Faded", "Alan Walker")[:8], "You were")
+		self.assertEqual(get_lyrics_google("Radioactive", "Imagine Dragons")[:9], "Whoa, oh,")
+		self.assertEqual(get_lyrics_google("Battle Symphony", "Linkin Park")[:7], "I got a")
 
 	def test_that_get_lyrics_does_not_break_with_wrong_data(self):
 		"""
-		Test that get_lyrics function does not break with wrong data
+		Test that get_lyrics_genius, get_lyrics_google, get_lyrics_lyricsmode functions don't break with wrong data
 		"""
-		self.assertEqual(get_lyrics(
-			"Battle Symphony", "One Direction", False), "Couldn't get lyrics for Battle Symphony by One Direction.\n")
-		self.assertEqual(get_lyrics("Faded", "Muhmello", False), "Couldn't get lyrics for Faded by Muhmello.\n")
-		self.assertEqual(get_lyrics("Battle Symphony", "Drake", False), "Couldn't get lyrics for Battle Symphony by Drake.\n")
+		assert get_lyrics_genius("Battle Symphony", "One Direction") is None
+		assert get_lyrics_genius("Faded", "Muhmello") is None
+		assert get_lyrics_genius("Battle Symphony", "Drake") is None
 
-		# Deleting above songs and artists from unsupported.txt
-		with open("unsupported.txt", "r") as f:
-			lines = f.readlines()
-		with open("unsupported.txt", "w") as f:
-			for line in lines:
-				if line not in [" Battle Symphony by One Direction \n", " Faded by Muhmello \n", " Battle Symphony by Drake \n"]:
-					f.write(line)
+		assert get_lyrics_lyricsmode("Battle Symphony", "One Direction") is None
+		assert get_lyrics_lyricsmode("Faded", "Muhmello") is None
+		assert get_lyrics_lyricsmode("Battle Symphony", "Drake") is None
+
+		assert get_lyrics_google("Battle Symphony", "One Direction") is None
+		assert get_lyrics_google("Faded", "Muhmello") is None
+		assert get_lyrics_google("Battle Symphony", "Drake") is None
+
+
 
 	def test_that_lyrics_works_for_unsupported_songs(self):
 		"""
 		Test that lyrics function gives 'unsupported' message to unsupported files
 		"""
-		get_lyrics("Hello", "World", False)
-		self.assertEqual(lyrics("Hello", "World"), "Lyrics unavailable for Hello by World.\n")
-		get_lyrics("Foo", "Bar", False)
-		self.assertEqual(lyrics("Foo", "Bar"), "Lyrics unavailable for Foo by Bar.\n")
-		get_lyrics("Fantastic", "Beasts", False)
-		self.assertEqual(lyrics("Fantastic", "Beasts"), "Lyrics unavailable for Fantastic by Beasts.\n")
+		self.assertEqual(lyrics("Hello", "World", False), "Couldn't get lyrics for Hello by World.\n")
+		self.assertEqual(lyrics("Foo", "Bar", False), "Couldn't get lyrics for Foo by Bar.\n")
+		self.assertEqual(lyrics("Fantastic", "Beasts", False), "Couldn't get lyrics for Fantastic by Beasts.\n")
 
 		# Deleting above songs and artists from unsupported.txt
 		with open("unsupported.txt", "r") as f:
@@ -91,14 +129,6 @@ class Tests(unittest.TestCase):
 			for line in lines:
 				if line not in [" Hello by World \n", " Foo by Bar \n", " Fantastic by Beasts \n"]:
 					f.write(line)
-
-	@patch('swaglyrics.cli.get_lyrics')
-	def test_that_lyrics_calls_get_lyrics(self, mock):
-		"""
-		test that lyrics function calls get_lyrics function
-		"""
-		lyrics("Alone", "Marshmello")
-		self.assertTrue(mock.called)
 
 	def test_that_lyrics_do_not_break_with_file_not_found(self):
 		"""
@@ -111,33 +141,37 @@ class Tests(unittest.TestCase):
 		"""
 		test that the database set on pythonanywhere is working and giving strippers for unsupported songs
 		"""
-		self.assertEqual(get_lyrics("Bitch Lasagna", "Party in Backyard")[:7], "[Intro]")
+		self.assertEqual(get_lyrics_genius("Bitch Lasagna", "Party in Backyard")[:7], "[Intro]")
 
 	@mock.patch('requests.post', return_value=R())
-	def test_that_get_lyrics_does_not_break_with_request_giving_wrong_status_code(self, mock_requests):
+	def test_that_unsupported_song_addition_does_not_break_with_request_giving_wrong_status_code(self, mock_requests):
 		"""
-		Test the get_lyrics does not break with requests giving wrong status code
+		Test the unsupported_song_addition does not break with requests giving wrong status code
 		"""
-		self.assertEqual(get_lyrics("Ki", "Ki", True), "Couldn\'t get lyrics for Ki by Ki.\n")
+		self.assertEqual(unsupported_song_addition("Ki", "Ki", True), "Couldn\'t get lyrics for Ki by Ki.\n")
+
 
 	@mock.patch('requests.post', side_effect=requests.exceptions.RequestException)
-	def test_that_get_lyrics_do_not_break_with_error_in_request(self, mock_requests):
+	def test_that_unsupported_song_addition_do_not_break_with_error_in_request(self, mock_requests):
 		"""
-		Test the get_lyrics does not break with error in requests
+		Test the unsupported_song_addition does not break with error in requests
 		"""
-		self.assertEqual(get_lyrics("Ki", "Ki", True), "Couldn\'t get lyrics for Ki by Ki.\n")
+		self.assertEqual(unsupported_song_addition("Ki", "Ki", True), "Couldn\'t get lyrics for Ki by Ki.\n")
 
 	@mock.patch('requests.post', return_value=R(200, "Season 3 is supernatural"))
-	def test_that_get_lyrics_calls_requests(self, mock_requests):
+	def test_that_unsupported_song_addition_calls_requests(self, mock_requests):
 		"""
-		Test that get lyrics calls requests
+		Test that unsupported_song_addition calls requests
 		"""
-		self.assertEqual(get_lyrics("River", "Dale", True), "Couldn't get lyrics for River by Dale.\nSeason 3 is supernatural")
+		self.assertEqual(unsupported_song_addition("River", "Dale", True), "Couldn't get lyrics for River by Dale.\nSeason 3 is supernatural")
+
+
 
 	@patch('os.system')
 	def test_clear(self, mock):
 		clear()
 		self.assertTrue(mock.called)
+
 
 if __name__ == '__main__':
 	unittest.main()
