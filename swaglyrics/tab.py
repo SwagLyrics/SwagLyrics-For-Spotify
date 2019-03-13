aaa4583f850ba8a2337f648a3f29ed519bee803e
TronoKurelz <34759498+TarunKurella@users.noreply.github.com>
2019-03-13 18:17:09 +0530

    Supports requests from chrome browser

--- swaglyrics/tab.py
+++ swaglyrics/tab.py
@@ -1,21 +1,42 @@
 from swaglyrics.cli import lyrics
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+from flask_cors import CORS, cross_origin
 import os
 from swaglyrics import spotify
 
+
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath('SwagLyrics-For-Spotify')), 'swaglyrics/templates'))
-# use relative path of the template folder
+
+CORS(app, support_credentials=True)
+
 
 song = None
 artist = None
+isChrome = False
+chromeSong=None
+isSameSong=True
 
-
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
+@cross_origin(supports_credentials=True)
 def tab():
     # format lyrics for the browser tab template
-    global song, artist
-    song = spotify.song()
-    artist = spotify.artist()
+    global song, artist,isChrome,isSameSong
+    isSameSong=True
+    if request.method=="POST" or request.method=="OPTIONS" :
+        isChrome=True
+        isSameSong=False
+        response=request.get_json(force=True)
+        song=response["title"]
+        artist=response["artist"]
+        
+        
+        
+    
+    if isChrome==False:
+        song = spotify.song()
+        artist = spotify.artist()    
+    
+    
     current_lyrics = lyrics(song, artist)
     current_lyrics = current_lyrics.split('\n')  # break lyrics line by line
     return render_template('lyrics.html', lyrics=current_lyrics, song=song, artist=artist)
@@ -24,9 +45,12 @@ def tab():
 @app.route('/songChanged', methods=['GET'])
 def song_changed():
     # to refresh lyrics when song changed
-    global song
-    if song == spotify.song() or spotify.song() is None:
-        return 'no'
+    global song,isChrome,chromeSong
+    if isChrome==False:
+        if song == spotify.song() or spotify.song() is None:
+           return 'no' 
+    if isChrome==True and isSameSong==True:
+        return 'no'       
     return 'yes'
 
 
