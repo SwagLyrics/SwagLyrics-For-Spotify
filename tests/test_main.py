f73503965882b856e4ce175e96e32bdbc4821868
TronoKurelz <34759498+TarunKurella@users.noreply.github.com>
2019-03-23 14:03:55 +0530

    updated test.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,7 +18,7 @@ class Tests(unittest.TestCase):
     def setup(self):
         pass
 
-    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=False, cli=False))
+    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=False, cli=False, chrome=False))
     def test_parser_prints_description(self, mock_argparse):
         """
         Tests whether prints its description
@@ -31,7 +31,7 @@ class Tests(unittest.TestCase):
 
     @mock.patch('threading.Timer', side_effect=None)
     @mock.patch('swaglyrics.tab.app.run', side_effect=None)
-    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=True, cli=False))
+    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=True, cli=False, chrome=False))
     def test_parser_runs_tab(self, mock_argparse, mock_app, mock_threader):
         """
         Tests whether parser runs tab
@@ -43,7 +43,7 @@ class Tests(unittest.TestCase):
         self.assertIn("Firing up a browser tab!", capturedOutput.getvalue())
         self.assertTrue(mock_app.called)
 
-    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=False, cli=True))
+    @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(tab=False, cli=True, chrome=False))
     def test_parser_runs_cli(self, mock_argparse):
         """
         Tests whether parser runs cli
