329f5f482d179c3310fbf10ee3883e3e117b6dc7
GermaVinsmoke <vaibhav1180@gmail.com>
2019-04-06 00:23:28 +0530

    Used autopep8 in __main__.py

--- swaglyrics/__main__.py
+++ swaglyrics/__main__.py
@@ -10,76 +10,85 @@ from swaglyrics.tab import app
 
 
 def main():
-	# 	print(r"""
-	#  ____                     _               _
-	# / ___|_      ____ _  __ _| |   _   _ _ __(_) ___ ___
-	# \___ \ \ /\ / / _` |/ _` | |  | | | | '__| |/ __/ __|
-	#  ___) \ V  V / (_| | (_| | |__| |_| | |  | | (__\__ \
-	# |____/ \_/\_/ \__,_|\__, |_____\__, |_|  |_|\___|___/
-	#                     |___/      |___/
-	# 	""")
-	print('Updating unsupported.txt from server.')
-	with open('unsupported.txt', 'w', encoding='utf-8') as f:
-		response = requests.get('http://aadibajpai.pythonanywhere.com/master_unsupported')
-		f.write(response.text)
-	print("Updated unsupported.txt successfully.")
-	program = "Swaglyrics"
-	parser = argparse.ArgumentParser(
-		prog=program,
-		usage='{prog} [options]'.format(prog=program),
-		description="Get lyrics for the currently playing song on Spotify. Either --tab or --cli is required.")
+    # 	print(r"""
+    #  ____                     _               _
+    # / ___|_      ____ _  __ _| |   _   _ _ __(_) ___ ___
+    # \___ \ \ /\ / / _` |/ _` | |  | | | | '__| |/ __/ __|
+    #  ___) \ V  V / (_| | (_| | |__| |_| | |  | | (__\__ \
+    # |____/ \_/\_/ \__,_|\__, |_____\__, |_|  |_|\___|___/
+    #                     |___/      |___/
+    # 	""")
+    print('Updating unsupported.txt from server.')
+    with open('unsupported.txt', 'w', encoding='utf-8') as f:
+        response = requests.get(
+            'http://aadibajpai.pythonanywhere.com/master_unsupported')
+        f.write(response.text)
+    print("Updated unsupported.txt successfully.")
+    program = "Swaglyrics"
+    parser = argparse.ArgumentParser(
+        prog=program,
+        usage='{prog} [options]'.format(prog=program),
+        description="Get lyrics for the currently playing song on Spotify."
+                    "Either --tab or --cli is required.")
 
-	# add_mutually_exclusive_group - makes sure either -c or -t is selected but not both
-	group = parser.add_mutually_exclusive_group()
-	group.add_argument('-t', '--tab', action='store_true', help='Display lyrics in a browser tab.')
-	group.add_argument('-c', '--cli', action='store_true', help='Display lyrics in the command-line.')
-	parser.add_argument('--song', help='Enter song name', type=str)
-	parser.add_argument('--artist', help='Enter artist name', type=str)
+    """
+    add_mutually_exclusive_group - makes sure either -c or -t is
+    selected but not both
+    """
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-t', '--tab', action='store_true',
+                       help='Display lyrics in a browser tab.')
+    group.add_argument('-c', '--cli', action='store_true',
+                       help='Display lyrics in the command-line.')
+    parser.add_argument('--song', help='Enter song name', type=str)
+    parser.add_argument('--artist', help='Enter artist name', type=str)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if args.tab:
-		print('Firing up a browser tab!')
-		app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-		app.static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-		port = 5042  # random
-		url = "http://127.0.0.1:{port}".format(port=port)
-		threading.Timer(1.25, lambda: webbrowser.open(url)).start()
-		app.run(port=port)
+    if args.tab:
+        print('Firing up a browser tab!')
+        app.template_folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'templates')
+        app.static_folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'static')
+        port = 5042  # random
+        url = "http://127.0.0.1:{port}".format(port=port)
+        threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+        app.run(port=port)
 
-	elif args.cli:
-		if args.song is None and args.artist is None:
-			song = spotify.song()  # get currently playing song
-			artist = spotify.artist()  # get currently playing artist
-		else:
-			song = args.song  # get song from command line argument
-			artist = args.artist  # get artist from command line argument
-			print(lyrics(song, artist))
-			raise SystemExit(0)
+    elif args.cli:
+        if args.song is None and args.artist is None:
+            song = spotify.song()  # get currently playing song
+            artist = spotify.artist()  # get currently playing artist
+        else:
+            song = args.song  # get song from command line argument
+            artist = args.artist  # get artist from command line argument
+            print(lyrics(song, artist))
+            raise SystemExit(0)
 
-		print(lyrics(song, artist))
-		print('\n(Press Ctrl+C to quit)')
-		while True:
-			# refresh every 5s to check whether song changed
-			# if changed, display the new lyrics
-			try:
-				if song == spotify.song() and artist == spotify.artist():
-					time.sleep(5)
-				else:
-					song = spotify.song()
-					artist = spotify.artist()
-					if song and artist is not None:
-						clear()
-						print(lyrics(song, artist))
-						print('\n(Press Ctrl+C to quit)')
-			except KeyboardInterrupt:
-				exit()
-			if os.environ.get("TESTING", "False") != "False":
-				break
+        print(lyrics(song, artist))
+        print('\n(Press Ctrl+C to quit)')
+        while True:
+            # refresh every 5s to check whether song changed
+            # if changed, display the new lyrics
+            try:
+                if song == spotify.song() and artist == spotify.artist():
+                    time.sleep(5)
+                else:
+                    song = spotify.song()
+                    artist = spotify.artist()
+                    if song and artist is not None:
+                        clear()
+                        print(lyrics(song, artist))
+                        print('\n(Press Ctrl+C to quit)')
+            except KeyboardInterrupt:
+                exit()
+            if os.environ.get("TESTING", "False") != "False":
+                break
 
-	else:
-		parser.print_help()
+    else:
+        parser.print_help()
 
 
 if __name__ == '__main__':
-	main()
+    main()
