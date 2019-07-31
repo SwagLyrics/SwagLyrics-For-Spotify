d9bc18d0e79242bd583e3228e258f89109a6cdb2
Aadi Bajpai <me@aadibajpai.me>
2019-08-01 02:48:53 +0530

    add backend_url variable
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- swaglyrics/__init__.py
+++ swaglyrics/__init__.py
@@ -3,3 +3,4 @@ import os
 name = 'swaglyrics'
 __version__ = '0.2.9'
 unsupported_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unsupported.txt')
+backend_url = 'https://aadibajpai.pythonanywhere.com'
