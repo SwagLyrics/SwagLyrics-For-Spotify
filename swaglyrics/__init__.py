bf300699afc13299e51e1e4bf72bc79e4fef5e5a
Aadi Bajpai <hello@aadibajpai.com>
2019-08-29 01:43:19 +0530

    prepare for v1.1.0
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- swaglyrics/__init__.py
+++ swaglyrics/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 name = 'swaglyrics'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 unsupported_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unsupported.txt')
 backend_url = 'https://aadibajpai.pythonanywhere.com'
 
