b4766a2854650ab5fd85f9dabb0cdf4fe9d6c00f
Aadi Bajpai <me@aadibajpai.me>
2019-08-30 08:16:47 +0530

    add tab tests
    testing route behaviours
    
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- swaglyrics/tab.py
+++ swaglyrics/tab.py
@@ -1,9 +1,9 @@
+import os
 from swaglyrics.cli import lyrics
 from flask import Flask, render_template
-import os
 from SwSpotify import spotify
 
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath('SwagLyrics-For-Spotify')), 'swaglyrics/templates'))
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 # use relative path of the template folder
 
 song = None
@@ -14,8 +14,7 @@ artist = None
 def tab():
     # format lyrics for the browser tab template
     global song, artist
-    song = spotify.song()
-    artist = spotify.artist()
+    song, artist = spotify.current()
     current_lyrics = lyrics(song, artist)
     current_lyrics = current_lyrics.split('\n')  # break lyrics line by line
     return render_template('lyrics.html', lyrics=current_lyrics, song=song, artist=artist)
