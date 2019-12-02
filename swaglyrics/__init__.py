617b2b95209b9379f90fdaa74a824335e7a80ef1
Aadi Bajpai <aadibajpai@gmail.com>
2019-12-02 19:00:25 +0530

    v1.1.1
    Updated backend url

--- swaglyrics/__init__.py
+++ swaglyrics/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 name = 'swaglyrics'
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 unsupported_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unsupported.txt')
-backend_url = 'https://aadibajpai.pythonanywhere.com'
+backend_url = 'https://api.swaglyrics.dev'
 
 
 class SameSongPlaying(Exception):
