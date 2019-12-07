1ead21b9cd59a2dc707ddfa657c60af0fb930c63
Eastan Giebler <kupa528@yahoo.com>
2019-12-07 13:14:41 -0600

    Add unit tests for non-latin titles
    - either song, artist, or both are completely non-latin

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -36,7 +36,6 @@ class Tests(unittest.TestCase):
 		self.assertEqual(stripper('Seasons (with Sjava & Reason)', 'Mozzy'), 'Mozzy-Sjava-and-Reason-Seasons')
 		self.assertEqual(stripper(
 			'거품 안 넘치게 따라줘 [Life Is Good] (feat. Crush, Dj Friz)', 'Dynamic Duo'), 'Dynamic-Duo-Life-Is-Good')
-		self.assertEqual(stripper('ハゼ馳せる果てるまで', 'ZUTOMAYO'), None)
 		self.assertEqual(stripper('Ice Hotel (ft. SZA)', 'XXXTENTACION'), 'XXXTENTACION-Ice-Hotel')
 
 	# Integration test
@@ -44,6 +43,9 @@ class Tests(unittest.TestCase):
 		"""
 		Test that get_lyrics function works
 		"""
+		self.assertEqual(get_lyrics('果てるまで', 'ハゼ馳せる'), None)  # song and artist non-latin
+		self.assertEqual(get_lyrics('Hello', 'ハゼ馳せる'), None)  # artist non-latin
+		self.assertEqual(get_lyrics('ハゼ馳せる果てるまで', 'ZUTOMAYO'), None)  # song non-latin
 		self.assertEqual(get_lyrics("Faded", "Alan Walker")[:9], "[Verse 1]")
 		self.assertEqual(get_lyrics("Radioactive", "Imagine Dragons")[:7], "[Intro]")
 		self.assertEqual(get_lyrics("Battle Symphony", "Linkin Park")[:9], "[Verse 1]")
