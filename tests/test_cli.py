178208cdfe6d1521d21d05a1ff09be361d9dbdd1
Sam Poder <39828164+sampoder@users.noreply.github.com>
2019-12-09 11:41:53 +0800

    Update tests/test_cli.py
    Co-Authored-By: Aadi Bajpai <me@aadibajpai.me>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -135,7 +135,7 @@ class Tests(unittest.TestCase):
 
 	@patch('swaglyrics.cli.requests.post', return_value=R(404))
 	@patch('swaglyrics.cli.get_lyrics', return_value=None)
-	def test_that_lyrics_requests_canfail(self, f_get_lyrics, mock_requests):
+	def test_that_lyrics_requests_can_fail(self, f_get_lyrics, mock_requests):
 		"""
 		Test that when get_lyrics calls requests and it does not succeed it doesn't append r.text
 		"""
