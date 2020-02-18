cbd5b368b9015a749721bfe4057a9adec5230921
Ananth <chessanu@gmail.com>
2020-02-18 20:47:51 +0530

    Revert "Added simple GUI using eel"
    This reverts commit bc1c659743c91bb36113307ebdeca5ba1891221f.

---
+++ swaglyrics/web/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+name = 'swaglyrics'
+__version__ = '1.1.1'
+unsupported_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unsupported.txt')
+backend_url = 'https://api.swaglyrics.dev'
+
+
+class SameSongPlaying(Exception):
+	pass
---
+++ swaglyrics/web/tab.py
@@ -0,0 +1,41 @@
+import os
+from swaglyrics.cli import lyrics
+from swaglyrics import SameSongPlaying
+from flask import Flask, render_template
+from SwSpotify import spotify, SpotifyNotRunning
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+# use relative path of the template folder
+
+song = None
+artist = None
+
+
+@app.route('/')
+def tab():
+    # format lyrics for the browser tab template
+    global song, artist
+    try:
+        song, artist = spotify.current()
+        current_lyrics = lyrics(song, artist)
+    except SpotifyNotRunning:
+        current_lyrics = 'Nothing playing at the moment.'
+    current_lyrics = current_lyrics.split('\n')  # break lyrics line by line
+    return render_template('lyrics.html', lyrics=current_lyrics, song=song, artist=artist)
+
+
+@app.route('/songChanged', methods=['GET'])
+def song_changed():
+    # to refresh lyrics when song changed
+    global song, artist
+    try:
+        if spotify.current() == (song, artist):
+            raise SameSongPlaying
+        else:
+            return 'yes'
+    except (SpotifyNotRunning, SameSongPlaying):
+        return 'no'
+
+
+if __name__ == '__main__':
+    app.run()
