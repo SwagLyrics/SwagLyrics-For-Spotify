a289cf1f447f6e9dbdada9a207fb199c9287ae11
Aadi Bajpai <hello@aadibajpai.com>
2020-06-04 03:32:01 +0530

    Add support for Bollywood
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -39,6 +39,7 @@ class Tests(unittest.TestCase):
 		self.assertEqual(stripper(
 			'거품 안 넘치게 따라줘 [Life Is Good] (feat. Crush, Dj Friz)', 'Dynamic Duo'), 'Dynamic-Duo-Life-Is-Good')
 		self.assertEqual(stripper('Ice Hotel (ft. SZA)', 'XXXTENTACION'), 'XXXTENTACION-Ice-Hotel')
+		self.assertEqual(stripper('Zikr (From "Amavas")', 'Armaan Malik'), 'Armaan-Malik-Zikr')
 
 	# Integration test
 	def test_that_get_lyrics_works(self):
