53d06f9b6e02eabaeddca4def52c5ef9787fd05a
Aadi Bajpai <me@aadibajpai.me>
2020-08-30 16:43:46 +0530

    Add lssts dataset and runner

---
+++ tests/lssts/lssts_runner.py
@@ -0,0 +1,9 @@
+import csv
+
+from swaglyrics.cli import stripper
+
+with open("lssts_dataset.tsv", newline='', encoding="utf-8") as csvfile:
+    f = csv.reader(csvfile, delimiter="\t")
+    for row in f:
+        # print(row)
+        assert stripper(row[0], row[1]) == row[2]
