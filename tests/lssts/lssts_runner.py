0de86ebc30110ca206d9add3eae8d71a335df869
Aadi Bajpai <hello@aadibajpai.com>
2020-09-01 10:32:43 +0530

    fix linking
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- tests/lssts/lssts_runner.py
+++ tests/lssts/lssts_runner.py
@@ -2,7 +2,7 @@ import csv
 
 from swaglyrics.cli import stripper
 
-with open("lssts_dataset.tsv", newline='', encoding="utf-8") as csvfile:
+with open("./lssts_dataset.tsv", newline='', encoding="utf-8") as csvfile:
     f = csv.reader(csvfile, delimiter="\t")
     for row in f:
         # print(row)
